dbd54f7b4a7ea3674b4718eb7e4b7fde4bf0c685
Rubin Khadka <rubin.robotic@gmail.com>
2024-11-13 19:41:47 +0100

    made changes to sth

--- task1_obtaindata.m
+++
@@ -1,8 +0,0 @@
-
-% Task 1: Obtain a data set
-
-wine_data = load("Data\small_wine\wine.data");
-
-wine_data.Properties.VariableNames = {'Alcohol', 'Malic acid', 'Ash', 'Alcalinity of ash' , 'Magnesium', 'Total phenols', 'Flavanoids', 'Nonflavanoid phenols',   'Proanthocyanins', 'Color intensity', 'Hue', 'OD280/OD315', 'Proline'};
-
-disp(wine_data);
