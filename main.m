41c1e121dff951aa54526427a4d21e574eeb0c5c
Rubin Khadka <rubin.robotic@gmail.com>
2024-11-15 11:51:04 +0100

    started kNN classifier

--- main.m
+++ main.m
@@ -10,13 +10,6 @@ addpath("scripts");
 % Obtain train and test dataset from large Wine dataset
 [x_train_Wine, y_train_Wine, x_test_Wine, y_test_Wine] = task1_obtainWinedata();
 
-% Test if the data has been splited properly
-disp(['Train MNIST size: ', num2str(size(x_train_MNIST, 1)), ' x ', num2str(size(x_train_MNIST, 2))]);
-disp(['Test MNIST size: ', num2str(size(x_test_MNIST, 1)), ' x ', num2str(size(x_test_MNIST, 2))]);
-
-disp(['Train Wine size: ', num2str(size(x_train_Wine, 1)), ' x ', num2str(size(x_train_Wine, 2))]);
-disp(['Test Wine size: ', num2str(size(x_test_Wine, 1)), ' x ', num2str(size(x_test_Wine, 2))]);
-
 %% Task 2: Build a kNN classifier
 
 
---
+++ scripts/task2_kNNclassifier.m
@@ -0,0 +1,50 @@
+%% Task 2: Build a kNN classifier
+
+% kNN Classifier Function
+function [predicted_labels, error_rate] = task2_kNNclassifier(train_data, train_labels, test_data, k, varargin)
+    
+    % Validate number of arguments
+    if nargin < 4
+        error("NO enough arguments. should be at least 4");
+    end
+    
+    % check size of train and test dimensions
+    if size(train_data, 2) ~= size(test_data, 2)
+        error("Number of columns (features) must match between train and test data.");
+    end
+    
+    % Validate value of k
+    num_train_samples = size(train_data, 1);
+    if k <= 0 || k > num_train_samples
+        error("k must be a positive integer and less than or equal to the number of training samples.");
+    end
+    
+    % Initialize predictions
+    num_test_samples = size(test_data, 1);
+    predicted_labels = zeros(num_test_samples, 1);
+    
+    % Loop over each test sample to find its k nearest neighbors
+    for i = 1:num_test_samples
+        % Compute Euclidean distance to all training samples
+        distances = sqrt(sum((train_data - test_data(i, :)) .^ 2, 2));
+        
+        % Sort distances and get indices of k nearest neighbors
+        [~, sorted_indices] = sort(distances);
+        nearest_neighbors = train_labels(sorted_indices(1:k));
+        
+        % Assign the most frequent label among the neighbors
+        predicted_labels(i) = mode(nearest_neighbors);
+    end
+    
+    % Calculate error rate if true test labels are provided
+    if nargin == 5
+        test_labels = varargin{1};
+        if length(test_labels) ~= num_test_samples
+            error("Length of test_labels must match the number of test samples.");
+        end
+        num_errors = sum(predicted_labels ~= test_labels);
+        error_rate = num_errors / num_test_samples;
+    else
+        error_rate = NaN; % No error rate calculated if test_labels not provided
+    end
+end
